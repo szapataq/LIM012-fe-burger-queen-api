278f8edcf25f089cf2ea488b6059cef125d2abe6
szapataq <szapata013@gmail.com>
2020-06-30 12:53:13 -0500

    Creacion de ruta Auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const MongoLib = require('../lib/mongo');
+const { jwtScret } = require('../config');
 
 const { secret } = config;
+const connector = new MongoLib();
 
 /** @module auth */
 module.exports = (app, nextMain) => {
@@ -17,15 +20,28 @@ module.exports = (app, nextMain) => {
    * @code {400} si no se proveen `email` o `password` o ninguno de los dos
    * @auth No requiere autenticación
    */
+
   app.post('/auth', (req, resp, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
       return next(400);
     }
-
     // TODO: autenticar a la usuarix
-    next();
+    connector.getUser('test', email)
+      .then((doc) => {
+        if (!doc) {
+          next(400);
+        } else if (password !== doc.password) {
+          next(400);
+        } else {
+          const token = jwt.sign({ uid: doc._id }, jwtScret);
+          resp.json({
+            success: true,
+            token,
+          });
+        }
+      });
   });
 
   return nextMain();
