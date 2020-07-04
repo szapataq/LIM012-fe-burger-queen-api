a0d6c8614c05aedec8909382a305dfc2ea97e37c
szapataq <szapata013@gmail.com>
2020-07-03 20:07:35 -0500

    adicion de metodos usados en la ruta users

--- controller/users.js
+++ controller/users.js
@@ -1,11 +1,51 @@
+const bcrypt = require('bcrypt');
 const MongoLib = require('../lib/mongo');
 
 const connector = new MongoLib();
 
 module.exports = {
   getUsers: async (req, resp, next) => {
-    const allUsers = await connector.getAll('users');
+    const limit = 10;
+    // let page = req.query.page || 1;
+    const allUsers = await connector.getAll('users')
+      .limit(limit);
     resp.send(allUsers);
     // next();
+  },
+
+  getOneUser: async (req, resp) => {
+    const paramId = req.params.uid;
+    const oneUser = await connector.get('users', paramId);
+    resp.send(oneUser);
+  },
+
+  createUser: async (req, resp, next) => {
+    const data = {
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+      roles: req.body.roles,
+    };
+    const uid = await connector.create('users', data);
+    const user = await connector.get('users', uid);
+    resp.status(201).send(user);
+  },
+
+  updateUser: async (req, resp) => {
+    const paramId = req.params.uid;
+    const data = {
+      email: req.body.email,
+      password: req.body.password,
+      roles: req.body.roles,
+    };
+    const uid = await connector.update('users', paramId, data);
+    const user = await connector.get('users', uid);
+    resp.send(user);
+  },
+
+  deleteUser: async (req, resp, next) => {
+    const paramId = req.params.uid;
+    const user = await connector.get('users', paramId);
+    await connector.delete('users', paramId);
+    resp.send(user);
   },
 };
