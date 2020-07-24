89a7e678b8cbac45e21688f8bf2f4a77f89f5378
szapataq <szapata013@gmail.com>
2020-07-23 23:46:00 -0500

    test unitarios de usuarios

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,32 +1,9 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const {
   getUsers, getOneUser, createUser, updateUser, deleteUser,
 } = require('../users');
 
 describe('getUsers', () => {
-  it('should get users collection', (done) => {
-    done();
-  });
-});
-
-describe('getOneUser', () => {
-  it('should get users collection', (done) => {
-    done();
-  });
-});
-
-describe('createUser', () => {
-  it('should get users collection', (done) => {
-    done();
-  });
-});
-
-describe('updateUser', () => {
-  it('should get users collection', (done) => {
-    done();
-  });
-});
-
-describe('DeleteUser', () => {
   it('should get users collection', (done) => {
     done();
   });
